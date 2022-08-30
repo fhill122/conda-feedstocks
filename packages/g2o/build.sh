5dd586d5dbb3aa23857647e13a5b334ecafd884d
IvanB <fhill122@gmail.com>
2022-08-30 14:56:11 +0800

    [g2o] remove opengl

--- packages/g2o/build.sh
+++ packages/g2o/build.sh
@@ -6,7 +6,7 @@ cd build
 cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
+      -DG2O_USE_CHOLMOD=OFF \
+      -DG2O_USE_OPENGL=OFF
 
 cmake --build . --config Release --target install
